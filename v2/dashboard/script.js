d4a13a7c41895df47908fc08b63616dca92a0219
Omolara5861 <debbiegterra@gmail.com>
2023-12-21 18:15:50 +0100

    feat: :sparkles: Add changes

--- v2/dashboard/script.js
+++ v2/dashboard/script.js
@@ -20,3 +20,17 @@ function showSweetAlert() {
     }
   })
 }
+
+jQuery(document).ready(function() {
+
+  $(".chat-list a").click(function() {
+      $(".chatbox").addClass('showbox');
+      return false;
+  });
+
+  $(".chat-icon").click(function() {
+      $(".chatbox").removeClass('showbox');
+  });
+
+
+});
